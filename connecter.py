8a55627ff72d397a8314ce7da375384782461c3b
Sankarsan Kampa <19631364+k3rn31p4nic@users.noreply.github.com>
2018-09-23 13:56:07 +0000

    connector: initial commit

---
+++ connecter.py
@@ -0,0 +1,69 @@
+import configparser
+import sys
+import os
+from selenium import webdriver
+
+config = configparser.ConfigParser()
+config.read('configurations.ini')
+
+
+def get_config(section, field):
+    if section in config and field in config[section]:
+        return config[section][field]
+    else:
+        return None
+
+
+def get_default_browser(platform):
+    if platform.startswith('linux'):
+        return 'firefox'
+    elif platform == 'darwin':
+        return 'safari'
+    elif platform == 'win32':
+        return 'ie'
+    else:
+        return None
+
+
+def get_webdriver(browser):
+    browser = browser.lower()
+
+    if not browser:
+        browser = get_default_browser(sys.platform)
+
+    if browser == 'chrome':
+        return webdriver.Chrome()
+    elif browser == 'firefox':
+        return webdriver.Firefox()
+    elif browser == 'ie' or browser == 'internetexplorer':
+        return webdriver.Ie()
+    elif browser == 'opera':
+        return webdriver.Opera()
+    elif browser == 'safari':
+        return webdriver.Safari()
+    else:
+        return None
+
+
+captive_portal_url = get_config('Environment', 'captive_portal_url') \
+    or 'http://localhost'
+username_field_name = get_config('Environment', 'username_field_name') \
+    or 'username'
+password_field_name = get_config('Environment', 'password_field_name') \
+    or 'password'
+login_button_name = get_config('Environment', 'login_button_name') \
+    or 'login'
+username = get_config('Credentials', 'username')
+password = get_config('Credentials', 'password')
+webdriver = get_webdriver(get_config('Default', 'browser'))
+
+
+if not webdriver:
+    print("Error:\tUnable to bind to any browser.")
+    print("\tPlease specify a browser in the configurations.ini file.")
+
+    print("\nExiting...")
+    sys.exit(1)
+
+
+webdriver.close()
